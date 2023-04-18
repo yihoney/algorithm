4b6e76201ced35adf9aeaa0b006edc76facdd37d
yihoney <109622@naver.com>
2023-04-18 23:33:21 +0900

    11번째 문제 통과

---
+++ inflearn/src/main/java/lecture01/problem11/Main.java
@@ -0,0 +1,30 @@
+package lecture01.problem11;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.next();
+        System.out.println(solution(str));
+    }
+
+    private static String solution(String str) {
+        int cnt = 1;
+        String ans = "";
+        str += " ";
+
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i - 1) != str.charAt(i)) {
+                ans += str.charAt(i - 1);
+                if (cnt > 1) {
+                    ans += String.valueOf(cnt);
+                }
+                cnt = 1;
+            } else {
+                cnt++;
+            }
+        }
+        return ans;
+    }
+}
