6971908d662ae2773a34790c1fe707025507f5b7
yihoney <109622@naver.com>
2023-05-01 21:07:01 +0900

    1181번 문제 통과

---
+++ baekjoon/src/main/java/string/problem1181/Main.java
@@ -0,0 +1,39 @@
+package string.problem1181;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String[] strArr = new String[num];
+        for (int i = 0; i < num; i++) {
+            strArr[i] = br.readLine();
+        }
+        System.out.println(solution(num, strArr));
+    }
+
+    private static String solution(int num, String[] strArr) {
+
+        Arrays.sort(strArr, (s1, s2) -> {
+            if (s1.length() == s2.length()) {
+                return s1.compareTo(s2);
+            } else {
+                return s1.length() - s2.length();
+            }
+        });
+
+        StringBuilder ans = new StringBuilder();
+
+        ans.append(strArr[0]).append("\n");
+
+        for (int i = 1; i < num; i++) {
+            if (!strArr[i - 1].equals(strArr[i])) {
+                ans.append(strArr[i]).append('\n');
+            }
+        }
+
+        return ans.toString();
+    }
+}
