234a92219b0140b69c109b3a2b1f98559adcb944
yihoney <109622@naver.com>
2023-05-01 22:12:18 +0900

    1541번 통과

---
+++ baekjoon/src/main/java/string/problem1541/Main.java
@@ -0,0 +1,32 @@
+package string.problem1541;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(solution(str));
+    }
+
+    private static int solution(String str) {
+        int ans = Integer.MAX_VALUE;
+        String[] arr = str.split("-");
+
+        for (int i = 0; i < arr.length; i++) {
+            int tmp = 0;
+            String[] tmpArr = arr[i].split("\\+");
+
+            for (String s : tmpArr) {
+                tmp += Integer.parseInt(s);
+            }
+
+            if (ans == Integer.MAX_VALUE) {
+                ans = tmp;
+            } else {
+                ans -= tmp;
+            }
+        }
+        return ans;
+    }
+}
