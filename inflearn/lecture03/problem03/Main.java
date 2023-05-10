99e824ce38321ded4851bbd1d0692025ad5c3f36
yihoney <109622@naver.com>
2023-05-10 22:05:10 +0900

    3번 문제 통과

---
+++ inflearn/lecture03/problem03/Main.java
@@ -0,0 +1,33 @@
+package inflearn.lecture03.problem03;
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n + 1];
+        arr[0] = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(n, k, arr));
+    }
+
+    private static int solution(int n, int k, int[] arr) {
+        int ans = Integer.MIN_VALUE;
+        int tmp = 0;
+        for (int i = k; i <= n; i++) {
+            tmp = arr[i] - arr[i - k];
+            if (tmp > ans) {
+                ans = tmp;
+            }
+        }
+        return ans;
+    }
+}
