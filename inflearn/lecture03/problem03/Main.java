faa40efeb9195de7d570591321c0315bc82f430a
yihoney <109622@naver.com>
2023-05-11 00:14:33 +0900

    3번 - 구간합

--- inflearn/lecture03/problem03/Main.java
+++ inflearn/lecture03/problem03/Main.java
@@ -2,7 +2,6 @@ package inflearn.lecture03.problem03;
 
 import java.io.*;
 import java.util.StringTokenizer;
-import java.util.stream.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
