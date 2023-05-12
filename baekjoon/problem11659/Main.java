f8cec373f44da0ebde3966c8772c4d4914c14a99
yihoney <109622@naver.com>
2023-05-13 00:42:11 +0900

    백준 11659번 통과

---
+++ baekjoon/problem11659/Main.java
@@ -0,0 +1,27 @@
+package baekjoon.problem11659;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[n + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            arr[i] = arr[i - 1] + Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int lt = Integer.parseInt(st.nextToken());
+            int rt = Integer.parseInt(st.nextToken());
+            System.out.println(arr[rt] - arr[lt - 1]);
+        }
+    }
+}
