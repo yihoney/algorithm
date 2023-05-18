e02efce7f353d0bf3808254fca466bbd6d64179b
yihoney <109622@naver.com>
2023-05-18 23:42:48 +0900

    2번 문제 통과

---
+++ inflearn/lecture04/problem02/Main.java
@@ -0,0 +1,28 @@
+package inflearn.lecture04.problem02;
+
+import java.io.*;
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str1 = br.readLine();
+        String str2 = br.readLine();
+        System.out.println(solution(str1, str2));
+    }
+
+    private static String solution(String str1, String str2) {
+        String ans = "YES";
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : str1.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        for (char c : str2.toCharArray()) {
+            if (!map.containsKey(c) || map.get(c) == 0) {
+                return "NO";
+            }
+            map.put(c, map.get(c) - 1);
+        }
+        return ans;
+    }
+}
