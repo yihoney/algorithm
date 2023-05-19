b355b396f3285584154cbaa18ed81afe4316c21a
yihoney <109622@naver.com>
2023-05-20 00:59:14 +0900

    3번 통과

--- inflearn/lecture04/problem03/Main.java
+++ inflearn/lecture04/problem03/Main.java
@@ -20,13 +20,18 @@ public class Main {
     private static String solution(int n, int k, int[] arr) {
         StringBuilder ans = new StringBuilder();
         HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < k; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        ans.append(map.size());
         int lt = 0;
-        for (int rt = k; rt <= n; rt++) {
-            for (int i = lt; i < rt; i++) {
-                map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        for (int rt = k; rt < n; rt++) {
+            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
+            map.put(arr[lt], map.getOrDefault(arr[lt], 0) - 1);
+            if (map.get(arr[lt]) <= 0) {
+                map.remove(arr[lt]);
             }
-            ans.append(map.size() + " ");
-            map.clear();
+            ans.append(" " + map.size());
             lt++;
         }
         return ans.toString();
