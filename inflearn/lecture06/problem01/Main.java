de8a6063010408c1a6cc63bf061c52f1e75365d2
yihoney <109622@naver.com>
2023-05-30 00:51:13 +0900

    1번 통과

---
+++ inflearn/lecture06/problem01/Main.java
@@ -0,0 +1,35 @@
+package inflearn.lecture06.problem01;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(n, arr));
+    }
+
+    private static String solution(int n, int[] arr) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[minIdx] > arr[j]) {
+                    minIdx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = tmp;
+            sb.append(arr[i] + " ");
+        }
+
+        return sb.toString();
+    }
+}
