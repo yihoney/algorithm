b4c773edf2c4a27be146258baff491b34a4bcd9d
yihoney <109622@naver.com>
2023-06-02 23:42:48 +0900

    5번 문제 통과

---
+++ inflearn/lecture06/problem05/Main.java
@@ -0,0 +1,31 @@
+package inflearn.lecture06.problem05;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(n, arr));
+    }
+
+    private static char solution(int n, int[] arr) {
+        char ans = 'U';
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : arr) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        if (map.size() == n) {
+            return ans;
+        } else {
+            return 'D';
+        }
+    }
+}
