1dc9c0ac2ff36afd2b9faaa4c3534954baa26f4f
yihoney <109622@naver.com>
2023-06-04 00:20:50 +0900

    6번 문제 통과

---
+++ inflearn/lecture06/problem06/Main.java
@@ -0,0 +1,31 @@
+package inflearn.lecture06.problem06;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(n, arr));
+    }
+
+    private static String solution(int n, int[] arr) {
+        StringBuilder ans = new StringBuilder();
+        int[] tmp = arr.clone();
+        Arrays.sort(tmp);
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != tmp[i]) {
+                ans.append((i + 1) + " ");
+            }
+        }
+
+        return ans.toString();
+    }
+}
