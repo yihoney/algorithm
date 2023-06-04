a0f1f1654e1c3107e70d8d592fdbbad7d272ebe7
yihoney <109622@naver.com>
2023-06-04 18:17:01 +0900

    4번 문제 통과

---
+++ inflearn/lecture06/problem04/Main.java
@@ -0,0 +1,42 @@
+package inflearn.lecture06.problem04;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int s = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(s, arr));
+    }
+
+    private static String solution(int s, int[] arr) {
+        StringBuilder ans = new StringBuilder();
+
+        LinkedList<Integer> cache = new LinkedList<>();
+
+        for (int n : arr) {
+            if (cache.contains(n)) {
+                cache.remove(n);
+            }
+            cache.addFirst(n);
+
+            if (cache.size() > s) {
+                cache.removeLast();
+            }
+        }
+
+        for (int n : cache) {
+            ans.append(n + " ");
+        }
+
+        return ans.toString();
+    }
+}
