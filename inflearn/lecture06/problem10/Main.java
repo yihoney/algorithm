6facfa5cbf9df8e87e64a5e887c8d663281ea0f3
yihoney <109622@naver.com>
2023-06-10 23:56:48 +0900

    코드 작성중

---
+++ inflearn/lecture06/problem10/Main.java
@@ -0,0 +1,11 @@
+package inflearn.lecture06.problem10;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+    }
+}
