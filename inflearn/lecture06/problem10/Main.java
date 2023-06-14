6d0ff08bf56a9a3c3701cd1a79e6b65a3a263177
yihoney <109622@naver.com>
2023-06-14 22:42:27 +0900

    10번 문제 통과

--- inflearn/lecture06/problem10/Main.java
+++ inflearn/lecture06/problem10/Main.java
@@ -9,6 +9,40 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int c = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(n, c, arr));
+    }
 
+    private static int solution(int n, int c, int[] arr) {
+        int ans = 0;
+        Arrays.sort(arr);
+        int lt = 1;
+        int rt = arr[n - 1];
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (count(arr, mid) >= c) {
+                ans = mid;
+                lt = mid + 1;
+            } else {
+                rt = mid - 1;
+            }
+        }
+        return ans;
+    }
+
+    private static int count(int[] arr, int mid) {
+        int cnt = 1;
+        int ep = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - ep >= mid) {
+                cnt++;
+                ep = arr[i];
+            }
+        }
+        return cnt;
     }
 }
