e8b5aa4358d6666c0a5e4591223c62a86cfecf0d
yihoney <109622@naver.com>
2023-06-17 22:06:03 +0900

    이진트리순회: DFS

---
+++ inflearn/lecture07/problem05/Main.java
@@ -0,0 +1,36 @@
+package inflearn.lecture07.problem05;
+
+class Node {
+    int data;
+    Node lt, rt;
+
+    public Node(int val) {
+        data = val;
+        lt = null;
+        rt = null;
+    }
+}
+
+public class Main {
+    static Node root;
+
+    public static void main(String[] args) {
+        root = new Node(1);
+        root.lt = new Node(2);
+        root.rt = new Node(3);
+        root.lt.lt = new Node(4);
+        root.lt.rt = new Node(5);
+        root.rt.lt = new Node(6);
+        root.rt.rt = new Node(7);
+        DFS(root);
+    }
+
+    private static void DFS(Node root) {
+        if (root == null) {
+            return;
+        } else {
+            DFS(root.lt);
+            DFS(root.rt);
+        }
+    }
+}
