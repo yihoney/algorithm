7bf13e39942460e0cbcd4e25508545cc5dd10957
yihoney <109622@naver.com>
2023-07-09 23:57:51 +0900

    최빈수 코드 작성중

---
+++ swea/ex/Main.java
@@ -0,0 +1,23 @@
+package swea.ex;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        int n = 0;
+        while (n <= t) {
+            n = Integer.parseInt(br.readLine());
+
+            System.out.println("#" + n + " " + solution(arr));
+        }
+    }
+
+    public static int solution(int[] arr) {
+        int ans = 0;
+
+        return ans;
+    }
+}
