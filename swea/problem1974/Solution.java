a3151bce9b249643483d492f3c8cbfa5385e2499
yihoney <109622@naver.com>
2023-07-15 23:10:14 +0900

    스도쿠 코드 개선

--- swea/problem1974/Solution.java
+++ swea/problem1974/Solution.java
@@ -8,9 +8,6 @@ import java.io.*;
  * 
  *         <pre>
  *         문제 - SWEA 1974 (스도쿠검증)
- *         풀이방법
- *         1. 수를 입력 받을 때부터 prefix sum을 미리 구해 배열에 저장
- *         2.배열[종료 구간]에서 배열[시작 구간 - 1]을 빼주면 해당 구간의 합을 구함
  *         - 겹치는 숫자가 없을 경우 1, 그렇지 않을 경우 0 출력
  *         </pre>
  */
@@ -35,7 +32,6 @@ public class Solution {
         int ans = 1;
         HashSet<Integer> set1 = new HashSet<>();
         HashSet<Integer> set2 = new HashSet<>();
-        HashSet<Integer> set3 = new HashSet<>();
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (!set1.add(arr[i][j]) || !set2.add(arr[j][i])) {
@@ -49,12 +45,12 @@ public class Solution {
             for (int j = 0; j < 9; j += 3) {
                 for (int k = 0; k < 3; k++) {
                     for (int l = 0; l < 3; l++) {
-                        if (!set3.add(arr[k + i][l + j])) {
+                        if (!set1.add(arr[k + i][l + j])) {
                             return 0;
                         }
                     }
                 }
-                set3.clear();
+                set1.clear();
             }
         }
         return ans;
