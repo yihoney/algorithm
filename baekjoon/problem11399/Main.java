c981ee53e28143003eb7956814e9aabd07f8c81a
yihoney <109622@naver.com>
2023-07-25 23:02:19 +0900

    백준 11399 통과

---
+++ baekjoon/problem11399/Main.java
@@ -0,0 +1,36 @@
+package baekjoon.problem11399;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(arr));
+    }
+
+    private static int solution(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+        int ans = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] += arr[i - 1];
+            ans += arr[i];
+        }
+        return ans;
+    }
+}
