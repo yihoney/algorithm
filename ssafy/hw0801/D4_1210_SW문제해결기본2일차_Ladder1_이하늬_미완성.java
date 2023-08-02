9d825bb3440126d9ca16f63aa7ad251972e95e00
yihoney <109622@naver.com>
2023-08-02 22:42:41 +0900

    BJ 15650

---
+++ ssafy/hw0801/D4_1210_SW문제해결기본2일차_Ladder1_이하늬_미완성.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * @author 이하늬
+ * 
+ *         <pre>
+ *          메모리: KB, 시간: ms
+ *         </pre>
+ * 
+ * @author 이하늬
+ *
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
