b24c531112ac8f12b4eb86a4e4d2c313adc2c807
yi <109622@naver.com>
2023-08-18 15:34:03 +0900

    [Gold IV] Title: 알파벳, Time: 2820 ms, Memory: 296740 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static char[][] arr;
+	static int[][] dirs = new int[][] { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
+	static boolean[][] isVisited;
+	static HashSet<Character> set = new HashSet<>();
+	static int R, C, ans;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		R = Integer.parseInt(st.nextToken());
+		C = Integer.parseInt(st.nextToken());
+		arr = new char[R + 2][C + 2];
+		isVisited = new boolean[R + 2][C + 2];
+
+		for (int i = 1; i <= R; i++) {
+			String str = br.readLine();
+			for (int j = 1; j <= C; j++) {
+				arr[i][j] = str.charAt(j - 1);
+			}
+		}
+
+		ans = Integer.MIN_VALUE;
+		dfs(new int[] { 1, 1 }, 0);
+		System.out.println(ans);
+
+	}
+
+	public static void dfs(int[] pos, int cnt) {
+
+		int cx = pos[0];
+		int cy = pos[1];
+
+		if (set.contains(arr[cx][cy])) {
+			
+			ans = Math.max(ans, cnt);
+			return;
+			
+		} else {
+
+			for (int dir = 0; dir < dirs.length; dir++) {
+
+				set.add(arr[cx][cy]);
+
+				int nx = cx + dirs[dir][0];
+				int ny = cy + dirs[dir][1];
+
+				if (arr[nx][ny] == 0) {
+					continue;
+				}
+
+				dfs(new int[] { nx, ny }, cnt + 1);
+
+			}
+			set.remove(arr[cx][cy]);
+		}
+	}
+}
