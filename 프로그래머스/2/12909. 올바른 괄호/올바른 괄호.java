a5eb522fd4b4b36e742debb7e4870e3683fb18f3
이하늬 <109622@naver.com>
2024-11-24 20:58:35 +0900

    [level 2] Title: 올바른 괄호, Time: 6.04 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+
+        int cnt = 0;
+        
+        for (char c: s.toCharArray()) {
+            if ('(' == c) {
+               cnt++;
+            } else {
+                if (cnt == 0) {
+                    return false;
+                }
+                cnt--;
+            }
+        }
+        
+        if (cnt != 0) {
+            answer = false;
+        }
+        
+        return answer;
+    }
+}
